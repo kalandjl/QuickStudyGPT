305b02896e2975300043bebdfd34801b02ec70b7
Kalan Jarvs-Loewen <karj5903@gmail.com>
2024-09-19 00:12:32 -0700

    Implemented chakra ui, more specific ai prompts

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,60 +1,88 @@
 "use client"
-import { FormEvent, useRef } from "react"
+import { FormEvent, useRef, useState } from "react"
 import { getGPT } from "../lib/gpt"
+import { Button, Card, CardBody, Heading, Stack, Textarea } from "@chakra-ui/react"
+import { NotesIcon, QuizIcon } from "./icons"
 
 const Home = () => {
 
-    let notes = useRef(null)
+    let [questions, setQuestions] = useState([])
 
     const doForm = async (e: FormEvent<HTMLFormElement>) => {
 
         e.preventDefault()
 
         //@ts-ignore
-        const res = await getGPT(e.currentTarget.children[0].children[0].value)
+        const res = await getGPT(e.currentTarget.children[0].value)
 
-        console.log(res)
+        const mes = res.message.content
+
+
+        try {
+
+            let questionsArr = JSON.parse(mes)
+
+            setQuestions(questionsArr)
+
+        } catch(e) {
+
+            alert(`Couldn't parse response: ${e}`)
+        }
     }
 
     return (
         <>
             <header
             id="header"
-            className="bg-lime-200 pl-40 py-10 ">
+            className="px-40 py-10 bg-white">
                 <div  
                 id="heading-wrap"
-                className="">
-                    <h1 
+                className="grid place-items-center w-full">
+                    <Heading 
                     id="heading"
-                    className="text-4xl text-blue-400 font-semibold">
-                        Quick Study GPT
-                    </h1>
+                    className="text-7xl text-slate-700 font-extrabold"
+                    >
+                    Quick Study GPT
+                    </Heading>
+                    <Heading as='h2' size='2xl'
+                    className="text-teal-500 mt-10 font-semibold">
+                        Enter your notes below
+                    </Heading>
+                    
                 </div>
             </header>
             <main 
             id="main-division"
-            className="h-screen px-40 py-5 bg-green-50">
+            className="h-screen px-40 py-5 bg-white">
                 <form 
                 id="notes-form"
                 onSubmit={(e) => doForm(e)}>
+                    <Textarea
+                    size="lg"
+                    placeholder='Here is a sample placeholder'
+                    className="font-slate-600" />
                     <div 
-                    id="notes-input-wrap w-full h-64">
-                        <input 
-                        ref={notes}
-                        type="text" 
-                        name="Notes" 
-                        id="note-input" 
-                        placeholder="Enter Notes Here"
-                        className="w-full h-64 border-2 focus-visible:border-blue-400" />
+                    id="button-stack-wrap"
+                    className="grid place-items-center py-10">
+                        <Stack direction='row' spacing={4}>
+                            <Button type="submit" leftIcon={<QuizIcon />} colorScheme='teal' variant='solid'>
+                                Notes
+                            </Button>
+                            <Button type="submit" rightIcon={<NotesIcon />} colorScheme='teal' variant='outline'>
+                                Summarize
+                            </Button>
+                        </Stack>
                     </div>
-                    <div id="buffer" className="h-10"></div>
-                    <button
-                    id="form-submit-btn"
-                    type="submit"
-                    className="border-2 hover:border-blue-400 rounded-lg px-4 py-2 mr-6 bg-white">
-                        Submit Notes!
-                    </button>
                 </form>
+                {questions.map((question, i) => 
+                    <>
+                        <p 
+                        key={i}
+                        className="text-black">
+                            {`${i}. ${question}`}
+                        </p>
+                    </>
+                )}
             </main>
         </>
     )
--- server/dist/gpt.js
+++ server/dist/gpt.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: "./res/.env" });
 // Access the OpenAI API key from process.env
 const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey: apiKey });
@@ -11,7 +11,7 @@ export const doGPT = async (notes) => {
             { role: "system", content: "You are a helpful assistant." },
             {
                 role: "user",
-                content: `Summarize these notes: "${notes}"`,
+                content: `Make 10 hard questions based off of these notes, and formate the questions such as ["question 1", "question 2",]. I should be able to run JSON.parse on the raw response without error: "${notes}"`,
             },
         ],
     });
