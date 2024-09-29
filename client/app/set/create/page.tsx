93573d63d26ff76a402dbe5a6269247f0225e5ce
Kalan Jarvs-Loewen <karj5903@gmail.com>
2024-09-29 13:36:23 -0700

    Firebase functions initialized

--- client/app/set/create/page.tsx
+++ client/app/set/create/page.tsx
@@ -22,77 +22,73 @@ const Page: NextPage = () => {
 
     return (
         <>
-            {loading ? <>HELLO</> : ""}
             <Loading loading={loading} />
             <div id="header-wrap" className="px-40 py-20">
                 <header className="font-extrabold text-5xl text-slate-800">
                     Create Set
                 </header>
             </div>
-            <form>
-                <div id="textarea-wrap" className="grid place-items-center">
-                    <div id="textarea" className="w-4/5">
-                        <label 
-                        htmlFor="Notes" 
-                        className="block text-sm text-gray-500 dark:text-gray-300">
-                            Notes
-                        </label>
-                        <textarea 
-                        onChange={(e) => {setNotes(e.target.value)}}
-                        placeholder="Copy & Paste" 
-                        className="block  mt-2 w-full placeholder-gray-400/70 
-                        dark:placeholder-gray-500 rounded-lg border border-gray-200
-                        bg-white px-4 h-64 py-2.5 text-gray-700 focus:border-blue-400
-                        focus:outline-none focus:ring focus:ring-blue-300 
-                        focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 
-                        dark:text-gray-300 dark:focus:border-blue-300"></textarea>
-                    </div>
-                    <div id="section-button" className="w-4/5 pl-10">
-                        <div id="button-wrap">
-                            <button 
-                            className="w-full px-10 py-4 mt-6 tracking-wider text-white 
-                            uppercase transition-colors duration-300 transform bg-teal-600 
-                            rounded-lg lg:w-auto hover:bg-teal-500 focus:outline-none 
-                            focus:bg-teal-500 font-extrabold text-lg"
-                            onClick={async (e) => {
+            <div id="textarea-wrap" className="grid place-items-center">
+                <div id="textarea" className="w-4/5">
+                    <label 
+                    htmlFor="Notes" 
+                    className="block text-sm text-gray-500 dark:text-gray-300">
+                        Notes
+                    </label>
+                    <textarea 
+                    onChange={(e) => {setNotes(e.target.value)}}
+                    placeholder="Copy & Paste" 
+                    className="block  mt-2 w-full placeholder-gray-400/70 
+                    dark:placeholder-gray-500 rounded-lg border border-gray-200
+                    bg-white px-4 h-64 py-2.5 text-gray-700 focus:border-blue-400
+                    focus:outline-none focus:ring focus:ring-blue-300 
+                    focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 
+                    dark:text-gray-300 dark:focus:border-blue-300"></textarea>
+                </div>
+                <div id="section-button" className="w-4/5 pl-10">
+                    <div id="button-wrap">
+                        <button 
+                        className="w-full px-10 py-4 mt-6 tracking-wider text-white 
+                        uppercase transition-colors duration-300 transform bg-teal-600 
+                        rounded-lg lg:w-auto hover:bg-teal-500 focus:outline-none 
+                        focus:bg-teal-500 font-extrabold text-lg"
+                        onClick={async (e) => {
 
-                                
-                                setLoading(true)
+                            setLoading(true)
 
-                                const res = await getGPT(notes)
+                            const res = await getGPT(notes)
 
-                                setLoading(false)
+                            setLoading(false)
 
-                                const mes =  JSON.stringify(res.message.content)
+                            const mes =  JSON.stringify(res.message.content)
 
-                                // Use regex to make GPT response parsable
-                                let cleanedMes = mes
-                                    .replace("json", "")
-                                    .replace(/\\n/g, '')      
-                                    .replace(/\\\"/g, '"')       
-                                    .replace(/^\s+/g, '')  
-                                    .replace(/\s+$/g, '') 
-                                    .replace(/\\/g, '')      
-                                    .replace(/^"+|"+$/g, '')
-                                    .replace(/```/g, '');
+                            // Use regex to make GPT response parsable
+                            let cleanedMes = mes
+                                .replace("json", "")
+                                .replace(/\\n/g, '')      
+                                .replace(/\\\"/g, '"')       
+                                .replace(/^\s+/g, '')  
+                                .replace(/\s+$/g, '') 
+                                .replace(/\\/g, '')      
+                                .replace(/^"+|"+$/g, '')
+                                .replace(/```/g, '');
 
-                                router.push(`/set/?q="${cleanedMes}"`)
-                            }}>
-                                <div
-                                id="button-inner"
-                                className="grid grid-flow-col">
-                                    <Link href="/set/create">
-                                        Quiz Me
-                                    </Link>
-                                    <div className="pl-3">
-                                        <ArrowIcon />
-                                    </div>
+                            router.push(`/set/?q="${cleanedMes}"`)
+                        }}>
+                            <div
+                            id="button-inner"
+                            className="grid grid-flow-col">
+                                <Link href="/set/create">
+                                    Quiz Me
+                                </Link>
+                                <div className="pl-3">
+                                    <ArrowIcon />
                                 </div>
-                            </button>
-                        </div>
+                            </div>
+                        </button>
                     </div>
                 </div>
-            </form>
+            </div>
         </>
     )
 }
