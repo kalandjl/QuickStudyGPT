af73a20fc00593d635c8020a48f7c4cb94805449
kalandjl <karj5903@gmail.com>
2024-10-17 23:49:11 -0700

    Prototype about page

---
+++ client/app/about/page.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+
+    return (
+        <>
+            <main id="page-wrap" className="px-20 py-10 grid gap-6">
+                <div id="header-about">
+                    <h1 className="text-7xl font-extrabold text-stone-200 mb-2">About</h1>
+                </div>
+                <p className="text-stone-300 font-bold">
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Proin mus nisl lobortis lorem bibendum. Habitasse potenti interdum maecenas ad consectetur pretium id. Rhoncus euismod etiam mus laoreet, porta lacus. Donec eget feugiat fermentum sagittis platea magnis dignissim lacus. Aliquam quisque metus dignissim interdum conubia et in facilisis. Pellentesque sed mollis condimentum hac amet laoreet neque. Malesuada elementum nisi tortor eu orci eu vel. Facilisi lectus placerat fringilla ultrices purus duis tempus non. Placerat et vel gravida nisi sapien sem adipiscing. Efficitur ullamcorper purus morbi conubia commodo sit conubia suspendisse. Aliquam interdum hendrerit cubilia iaculis convallis adipiscing nascetur congue. Molestie mattis sed ligula cursus netus; elementum blandit faucibus. Varius diam praesent urna torquent tempus praesent. Sociosqu justo platea eros donec luctus. Blandit potenti fringilla non fames nascetur ultricies fringilla sollicitudin. Aconsequat feugiat efficitur; lacus scelerisque dignissim elementum. Porta fermentum ultrices eleifend pretium sociosqu volutpat euismod.
+                </p>
+            </main>
+        </>
+    )
+}
+
+export default Home
